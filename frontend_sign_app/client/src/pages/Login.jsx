0433eb348c73d09aca5ee4bb83b21bf2f5423b3e
ShreyasBhoge <shreyasbhoge111@gmail.com>
2025-07-05 18:59:19 +0530

    Update Login.jsx

--- frontend_sign_app/client/src/pages/Login.jsx
+++ frontend_sign_app/client/src/pages/Login.jsx
@@ -8,12 +8,13 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const API_BASE = process.env.REACT_APP_API_URL;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post(`${API_BASE}/api/auth/login`, {
         email,
         password,
       });
